bfa06b3599b2eee5a612374093eed54bda89bdee
Yujin Sung <rnlduadns98@naver.com>
2022-12-08 23:58:40 +0900

    fix  : 2-13
    소수 알고리즘

--- 2.JavaScript-Basic/2-13/primeNum.js
+++ 2.JavaScript-Basic/2-13/primeNum.js
@@ -1,12 +1,31 @@
-let n = 5;
-let sign = '';
+// let n = 5;
+// let sign = '';
 
-nextPrime: for(let i = 2; i<=n ; i++){
-    for(let j=2; j<i; j++){
-        if((i!=j)&&(i%j==0))continue nextPrime;
+// nextPrime: for(let i = 2; i<=n ; i++){
+//     for(let j=2; j<i; j++){
+//         if((i!=j)&&(i%j==0))continue nextPrime;
         
+//     }
+
+//     console.log(i);
+
+// }
+
+let n = 5;
+let i=2;
+let j=2;
+// j=2;
+nextPrime : while(i<=n){
+
+    j=2;
+
+    while(j<i){
+       
+       if((i!=j)&&(i%j==0))continue nextPrime;
+       j++;
     }
 
     console.log(i);
-
+    i++;
+    
 }

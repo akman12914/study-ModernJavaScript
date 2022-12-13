e320291ff613b52b5194d207031a65d9e7bb3f48
Yujin Sung <rnlduadns98@naver.com>
2022-12-14 05:12:00 +0900

    study: 2-17
    화살표 함수 기본

---
+++ 2.JavaScript-Basic/2-17/arrowFunction.js
@@ -0,0 +1,12 @@
+//화살표함수 사용
+
+let ask = (question, yes, no) => {
+    if (confirm(question)) yes()
+    else no();
+}
+
+ask(
+    "동의하십니까?",
+    () => { alert("동의하셨습니다."); },
+    () => { alert("취소 버튼을 누르셨습니다."); }
+  );

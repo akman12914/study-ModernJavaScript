164084541258242f3bc6020b96fbdaf134d8fae6
Yujin Sung <rnlduadns98@naver.com>
2022-12-18 23:56:56 +0900

    add:3-2
    코딩스타일
    
    eslint 설치

--- 2.JavaScript-Basic/2-13/effectiveValue.js
+++ 2.JavaScript-Basic/2-13/effectiveValue.js
@@ -1,5 +1,6 @@
 while(1){
-    effectValue = prompt('100보다 큰 숫자를 입력하세요', 0);
+
+    let effectValue = prompt('100보다 큰 숫자를 입력하세요', 0);
     
     if(effectValue>100 || effectValue == null || effectValue ==undefined )
     break;
--- 3.codeQuailty/3-3/comment.js
+++ 3.codeQuailty/3-3/comment.js
@@ -5,8 +5,11 @@
  * @param {number} n 곱할 횟수, 반드시 자연수여야 함
  * @return {number} x의 n 거듭제곱을 반환함
  */
+let number;
 
  function pow(x, n) {
     number = x**n;
     return number;
   }
+
+console.log(pow(2,3));

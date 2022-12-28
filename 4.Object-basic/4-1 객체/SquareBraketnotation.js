39bfa7675ef4b55055f305a5a1897f94963d4bb0
Yujin Sung <rnlduadns98@naver.com>
2022-12-29 06:10:46 +0900

    study: 4-1
    객체 - 대괄호 표기법

---
+++ 4.Object-basic/4-1 객체/SquareBraketnotation.js
@@ -0,0 +1,10 @@
+let bookStore = {};
+
+
+bookStore["Most Popular Book"] = 'Chain';
+
+console.log(bookStore["Most Popular Book"]);
+
+delete bookStore["Most Popular Book"];
+
+console.log(bookStore["Most Popular Book"]);

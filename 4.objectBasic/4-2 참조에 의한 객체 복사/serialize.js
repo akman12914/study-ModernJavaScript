7ca3990577fa6f021173473fa0bec83775a04a56
akman12914 <rnlduadns98@naver.com>
2023-01-12 16:35:36 +0900

    study: 4-2
    역직렬화

--- 4.objectBasic/4-2 참조에 의한 객체 복사/serialize.js
+++ 4.objectBasic/4-2 참조에 의한 객체 복사/serialize.js
@@ -1,15 +1,25 @@
-let Person1 = {
+
+let Person = {
     name : "하준",
     bestFriend : "신일"
 }
 
-let Person2 = {
-    name : "신일",
-    bestFriend : "하준"
-}
+let serializedBestFriend = JSON.stringify(Person);
 
-let serializedBestFriend = [Person1.bestFriend, Person2.bestFriend];
+    // 오류케이스 
+    // ex
+    // let serializedBestFrined = Person ; 
+    // JSON.stringify(serializedBestFriend);
 
-JSON.stringify(serializedBestFriend);
+    // Person의 메모리주소를 가리키는 변수가 하나 더 생기고, Person객체의 키값들을 직렬화하지는 못한다. 
+    // 프로퍼티를 복사하는 것만으론 객체를 복제할 수 없기 때문이다.
 
 console.log(serializedBestFriend);
+console.log(typeof serializedBestFriend);
+
+console.log("---------------------------");
+
+let deserializedBestFriend = JSON.parse(serializedBestFriend);
+
+console.log(deserializedBestFriend);
+console.log(typeof deserializedBestFriend);

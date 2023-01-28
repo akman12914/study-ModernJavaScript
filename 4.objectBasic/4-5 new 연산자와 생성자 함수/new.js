52a97ac91e546941fa6d68391b009f97326705b5
akman12914 <rnlduadns98@naver.com>
2023-01-28 23:47:09 +0900

    study: 4-5
    new 연산자 실행 시 생기는 일

--- 4.objectBasic/4-5 new 연산자와 생성자 함수/new.js
+++ 4.objectBasic/4-5 new 연산자와 생성자 함수/new.js
@@ -1,5 +1,10 @@
 function User(name){
+    //this = {}; (빈 객체가 임시적으로 만들어짐) //new를 실행했을 때
+
+    //새 프로퍼티를 this에 추가 
     this.name = name;
+
+    //return this; (this가 암시적으로 반환됨) //new를 실행했을 때
     
 }
     

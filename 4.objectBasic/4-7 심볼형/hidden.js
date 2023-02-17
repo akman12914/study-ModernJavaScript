cc25d8cc86dbf14d93732eefd83f3dd3241ded8a
akman12914 <rnlduadns98@naver.com>
2023-02-17 23:16:57 +0900

    study: 4-7
    심볼형 프로퍼티 숨기기

--- 4.objectBasic/4-7 심볼형/hidden.js
+++ 4.objectBasic/4-7 심볼형/hidden.js
@@ -8,5 +8,10 @@ let user = { // 서드파티 코드에서 가져온 객체
 
   
 //   user[id] = "제 3 스크립트 id 값";
+
+for (let key in user) console.log(key); // 심볼형 배제 
+
+let clone = Object.assign({}, user);
   
 console.log( user[id] ); // 심볼을 키로 사용해 데이터에 접근할 수 있습니다.
+console.log( clone[id] );

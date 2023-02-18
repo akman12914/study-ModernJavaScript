b8a27d1b5ace3f2cec21b92a2411658f84513627
akman12914 <rnlduadns98@naver.com>
2023-02-18 22:52:33 +0900

    study: 4-7
    전역심볼

--- 4.objectBasic/4-7 심볼형/hidden.js
+++ 4.objectBasic/4-7 심볼형/hidden.js
@@ -1,4 +1,7 @@
 let id = Symbol("id");
+let pw = Symbol.for("pw");
+let tempPw = Symbol.for("pw");
+
 
 let user = { // 서드파티 코드에서 가져온 객체
     name: "John",
@@ -15,3 +18,4 @@ let clone = Object.assign({}, user);
   
 console.log( user[id] ); // 심볼을 키로 사용해 데이터에 접근할 수 있습니다.
 console.log( clone[id] );
+console.log( pw === tempPw );

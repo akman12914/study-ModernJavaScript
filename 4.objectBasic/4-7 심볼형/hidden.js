1c5946abb4437eb2b858a914518984f6ea15b64f
akman12914 <rnlduadns98@naver.com>
2023-02-19 11:11:55 +0900

    study: 4-7
    Symbol.keyFor

--- 4.objectBasic/4-7 심볼형/hidden.js
+++ 4.objectBasic/4-7 심볼형/hidden.js
@@ -3,6 +3,7 @@ let pw = Symbol.for("pw");
 let tempPw = Symbol.for("pw");
 
 
+
 let user = { // 서드파티 코드에서 가져온 객체
     name: "John",
     [id]: 123
@@ -19,3 +20,7 @@ let clone = Object.assign({}, user);
 console.log( user[id] ); // 심볼을 키로 사용해 데이터에 접근할 수 있습니다.
 console.log( clone[id] );
 console.log( pw === tempPw );
+console.log( Symbol.keyFor(pw) );
+console.log( Symbol.keyFor(tempPw) );
+console.log( Symbol.keyFor(id) );
+console.log( id.description );

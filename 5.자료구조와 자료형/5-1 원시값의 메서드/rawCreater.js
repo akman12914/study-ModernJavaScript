e8c78bedf1fae6d003b84507bd326662700718a9
akman12914 <rnlduadns98@naver.com>
2023-03-05 21:39:42 +0900

    study: 5-1
    원시값의 메서드 추가학습

--- 5.자료구조와 자료형/5-1 원시값의 메서드/rawCreater.js
+++ 5.자료구조와 자료형/5-1 원시값의 메서드/rawCreater.js
@@ -1,2 +1,10 @@
 console.log( typeof 0);
 console.log( typeof new Number(0));
+
+
+let zero = new Number("123");
+let one = Number(0);
+
+console.log(zero); 
+console.log(one);
+// console.log(null.test); //error 발생. null/undefined는 메서드가 없다

40d149d458523da7b24b7b384a5ee00ee650b9ed
akman12914 <rnlduadns98@naver.com>
2023-03-08 18:30:26 +0900

    study: 5-2
    숫자 종류

--- 5.자료구조와 자료형/5-2 숫자형/number.js
+++ 5.자료구조와 자료형/5-2 숫자형/number.js
@@ -1,4 +1,11 @@
+console.log( 7.3e9 );  // 73억 (7,300,000,000)
 
 let billion = 1e9;  // 10억, 1과 9개의 0
+let ms = 1e-6;
+let hexadecimal = 0xff; // 255의 16진수
+let binary = 0b11111111;// 255의 2진수
+let oct = 0o377; //255 8진수
 
-console.log( 7.3e9 );  // 73억 (7,300,000,000)
+
+
+

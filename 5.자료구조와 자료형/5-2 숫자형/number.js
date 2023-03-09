858e431a4bd853f7f67b9bd16e7e59f7b7c9c612
akman12914 <rnlduadns98@naver.com>
2023-03-09 20:03:24 +0900

    study: 5-2
    toString(base)

--- 5.자료구조와 자료형/5-2 숫자형/number.js
+++ 5.자료구조와 자료형/5-2 숫자형/number.js
@@ -1,3 +1,5 @@
+"use strict"
+
 console.log( 7.3e9 );  // 73억 (7,300,000,000)
 
 let billion = 1e9;  // 10억, 1과 9개의 0
@@ -5,6 +7,15 @@ let ms = 1e-6;
 let hexadecimal = 0xff; // 255의 16진수
 let binary = 0b11111111;// 255의 2진수
 let oct = 0o377; //255 8진수
+
+//toString(base)
+console.log( hexadecimal.toString(16) );
+console.log( binary.toString(2) );
+console.log( 123423432423432..toString(36) );
+
+
+
+
 
 
 

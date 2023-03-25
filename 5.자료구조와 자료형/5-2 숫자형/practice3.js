0fa2d612869cbc602af5ec08515b704ba1e0fa79
akman12914 <rnlduadns98@naver.com>
2023-03-25 23:41:31 +0900

    study: 5-2
    숫자형 과제 3 :숫자를 입력할 때까지 반복하기

--- 5.자료구조와 자료형/5-2 숫자형/practice3.js
+++ 5.자료구조와 자료형/5-2 숫자형/practice3.js
@@ -5,5 +5,12 @@ function readNumber() {
 
     do {
         num = prompt("유효한 숫자값을 입력하세요 : ", 0);
-    }
+    } while ( !isFinite(num) );
+
+    if (num == null || num === '') return null;
+
+    return +num;
 }
+
+console.log(`Read : ${readNumber()}`);
+
